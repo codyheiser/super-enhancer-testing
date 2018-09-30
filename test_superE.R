695de7156e8c948125c04f2373b66287324201fc
Cody Heiser <codyheiser49@gmail.com>
2018-09-30 14:48:08 -0500

    cleaned up arguments for main function to ensure parameter bounds and formula are passed properly

--- test_superE.R
+++ test_superE.R
@@ -19,11 +19,11 @@ parser$add_argument('-i', '--iterations', nargs='+', default='2000',
 parser$add_argument('-f', '--formula', default='~E1+E2+E3+E4+E5+E6',
                     help='Formula describing enhancer interactions. Default ~E1+E2+E3+E4+E5+E6.')
 parser$add_argument('-ab', '--activitybounds',  default='c(-150, 150)',
-                    help='Activity bounds to pass to superEnhancerDataObject() function.')
+                    help='Activity bounds to pass to superEnhancerDataObject() function. Default [-150, 150].')
 parser$add_argument('-eb', '--errorbounds',  default='c(10^-3, 10^3)',
-                    help='Error parameter bounds to pass to superEnhancerDataObject() function.')
+                    help='Error parameter bounds to pass to superEnhancerDataObject() function. Default [0.001, 1000].')
 parser$add_argument('-sb', '--scalebounds',  default='c(10^-3, 10^3)',
-                    help='Scale parameter bounds to pass to superEnhancerDataObject() function.')
+                    help='Scale parameter bounds to pass to superEnhancerDataObject() function. Default [0.001, 1000].')
 # get command line options, if help encountered print help and exit,
 #   otherwise if options not found on command line, set defaults
 args <- parser$parse_args()
@@ -41,11 +41,13 @@ error.models <- c('gaussian','lognormal')
 link.functions <- c('additive','exponential','logistic')
 
 # define testing function
-test.model <- function(df, optim.iter, out = 'outputs/', enhancer.formula, activity.bounds = c(-150,150), error.bounds = c(10^-3, 10^3), scale.bounds = c(10^-3, 10^3), ...){
+test.model <- function(df, optim.iter, out = 'outputs/', enhancer.formula, activity.bounds = c(-150,150), error.bounds = c(10^-3, 10^3), scale.bounds = c(10^-3, 10^3)){
   # df = data in superE format
   # optim.iter = total number of iteration for optimization function to perform. can be list. 
   # out = path to output directory
-  # ... = additional parameters to pass to enhancerDataObject()
+  # enhancer.formula = interactions between variables to be modeled
+  # maxit = maximum iterations of optimDE() to run
+  # activity.bounds, error.bounds, scale.bounds = options to pass to enhancerDataObject() that restrict search for coefficients
   
   master.out <- data.frame() # initiate df for dumping fit parameters and BICs into
   
@@ -54,7 +56,8 @@ test.model <- function(df, optim.iter, out = 'outputs/', enhancer.formula, activ
       print(paste0('Performing ', iterations, ' iterations'))
     
       # run test using all six link/error function combos
-      result <- test.params(df, error.models, link.functions, maxit = iterations, enhancer.formula = enhancer.formula, ...) # build models using all error/link function combinations
+      result <- test.params(df, error.models, link.functions, maxit = iterations, enhancer.formula = enhancer.formula, 
+                            actBounds = activity.bounds, errBounds = error.bounds, scaleBounds = scale.bounds) # build models using all error/link function combinations
         
       # append important info to master.out df
       master.link <- data.frame()
@@ -87,5 +90,5 @@ test.model <- function(df, optim.iter, out = 'outputs/', enhancer.formula, activ
   write.csv(master.out, file = paste0(out,'superE_Results_',Sys.Date(),'.csv'), row.names = F)
 }
 
-test.model(df = datain, optim.iter = i, out = args$outputlocation,
-           enhancer.formula = f, activityParameterBounds = ab, errorParameterBounds = eb, scaleParameterBounds = sb)
+test.model(df = datain, optim.iter = i, out = args$outputlocation, enhancer.formula = f, 
+           activityParameterBounds = ab, errorParameterBounds = eb, scaleParameterBounds = sb)
